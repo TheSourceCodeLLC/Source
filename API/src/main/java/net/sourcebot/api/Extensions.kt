b5417c00586dd5fe6c8296c41ff0ba44356c71d8
Hunter Wignall <hwignall2000@gmail.com>
2020-06-08 01:59:18 -0400

    Account for weird member mentions

--- API/src/main/java/net/sourcebot/api/Extensions.kt
+++ API/src/main/java/net/sourcebot/api/Extensions.kt
@@ -12,7 +12,7 @@ import java.nio.charset.StandardCharsets
 fun String.urlEncoded(charset: Charset = StandardCharsets.UTF_8): String = URLEncoder.encode(this, charset)
 
 fun Arguments.nextMember(guild: Guild): Member? {
-    val target = next()?.replace("<@(\\d+)>".toRegex(), "$1") ?: return null
+    val target = next()?.replace("<@!?(\\d+)>".toRegex(), "$1") ?: return null
     try {
         val byId = guild.getMemberById(target)
         if (byId != null) return byId
