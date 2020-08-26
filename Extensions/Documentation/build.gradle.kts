cc404348d28eaedf036fbe964072464c6e3e4442
Hunter Wignall <hwignall2000@gmail.com>
2020-08-26 16:18:30 -0400

    Remove Kotlin plugin default dependency. Fixes #2 Remove Kotlin transitive dependency from JenkinsParser-Kotlin.

--- Extensions/Documentation/build.gradle.kts
+++ Extensions/Documentation/build.gradle.kts
@@ -3,7 +3,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.TheForbiddenAi:JenkinsParser-Kotlin:v1.3.1")
+    implementation("com.github.TheForbiddenAi:JenkinsParser-Kotlin:v1.3.1") {
+        exclude("org.jetbrains.kotlin")
+    }
     implementation("com.github.giflw:remark-java:v1.1.0")
     implementation("org.jsoup:jsoup:1.13.1")
 }
