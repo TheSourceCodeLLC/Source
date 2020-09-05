4edc21e969d7a1b23c54fd9da5b115eed638de56
Hunter Wignall <hwignall2000@gmail.com>
2020-09-05 10:00:03 -0400

    Configure Logback FileAppender with Daily log rotation

--- API/src/main/kotlin/net/sourcebot/api/logger/LoggerConfiguration.kt
+++ API/src/main/kotlin/net/sourcebot/api/logger/LoggerConfiguration.kt
@@ -2,8 +2,13 @@ package net.sourcebot.api.logger
 
 import ch.qos.logback.classic.*
 import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.Appender
 import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.encoder.Encoder
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+import java.nio.charset.StandardCharsets
 
 class LoggerConfiguration : BasicConfigurator() {
     private val loggerLevels = mapOf<String, Level>(
@@ -14,29 +19,63 @@ class LoggerConfiguration : BasicConfigurator() {
     )
 
     override fun configure(context: LoggerContext) {
-        val layout = PatternLayout().apply {
-            this.pattern = "[%highlight(%d{hh:mm:ss a z} %-5level)] [%highlight(%logger)] %msg%n"
-            this.context = context
-            start()
-        }
-        val encoder = LayoutWrappingEncoder<ILoggingEvent>().apply {
-            this.context = context
-            this.layout = layout
-        }
-        val appender = ConsoleAppender<ILoggingEvent>().apply {
-            this.context = context
-            this.name = "STDOUT"
-            this.isWithJansi = System.getProperty("useJansi")?.toBoolean() ?: false
-            setEncoder(encoder)
-            start()
-        }
         context.getLogger(Logger.ROOT_LOGGER_NAME).apply {
-            this.level = Level.INFO
-            addAppender(appender)
+            this.level = LOG_LEVEL
+            addAppender(consoleAppender(context))
+            addAppender(fileAppender(context))
         }
 
         loggerLevels.forEach { (name, level) ->
             context.getLogger(name).level = level
         }
+    }
+
+    private fun consoleAppender(context: LoggerContext): Appender<ILoggingEvent> {
+        val encoder = encoder(context, "[%highlight(%d{hh:mm:ss a z} %-5level)] [%highlight(%logger)] %msg%n")
+        return ConsoleAppender<ILoggingEvent>().apply {
+            this.isWithJansi = System.getProperty("useJansi")?.toBoolean() ?: false
+            this.context = context
+            this.name = "STDOUT"
+            setEncoder(encoder)
+            start()
+        }
+    }
+
+    private fun fileAppender(context: LoggerContext): Appender<ILoggingEvent> {
+        val encoder = encoder(context, "[%d{hh:mm:ss a z} %-5level] [%logger] %msg%n")
+        val rollingPolicy = TimeBasedRollingPolicy<ILoggingEvent>().apply {
+            this.fileNamePattern = "logs/%d{yyyy-MM-dd}.log.gz"
+            this.isCleanHistoryOnStart = true
+            this.context = context
+            this.maxHistory = 7
+        }
+        return RollingFileAppender<ILoggingEvent>().apply {
+            this.rollingPolicy = rollingPolicy.also {
+                it.setParent(this)
+                it.start()
+            }
+            this.context = context
+            this.encoder = encoder
+            this.name = "FILE"
+            start()
+        }
+    }
+
+    private fun encoder(context: LoggerContext, pattern: String): Encoder<ILoggingEvent> {
+        val layout = PatternLayout().apply {
+            this.pattern = pattern
+            this.context = context
+            start()
+        }
+        return LayoutWrappingEncoder<ILoggingEvent>().apply {
+            this.charset = StandardCharsets.UTF_8
+            this.context = context
+            this.layout = layout
+            start()
+        }
+    }
+
+    companion object {
+        @JvmStatic var LOG_LEVEL: Level = Level.INFO
     }
 }
