309b4f8f9d22262c8524b6b392b7e10fefd63647
Hunter Wignall <hwignall2000@gmail.com>
2020-09-15 10:46:02 -0400

    Process Arguments in AbstractMessageHandler

--- API/src/main/kotlin/net/sourcebot/api/event/AbstractMessageHandler.kt
+++ API/src/main/kotlin/net/sourcebot/api/event/AbstractMessageHandler.kt
@@ -3,6 +3,7 @@ package net.sourcebot.api.event
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.sourcebot.api.command.argument.Arguments
 
 /**
  * This class is responsible for processing command-like messages
@@ -28,57 +29,12 @@ abstract class AbstractMessageHandler constructor(
                 if (!content.startsWith(mention)) return
                 content.substring(mention.length)
             } else return
-        } else {
-            content.substring(prefix.length)
-        }
+        } else content.substring(prefix.length)
         if (content.isBlank()) return
-        val args = readArguments(content)
-        val label = args[0].toLowerCase()
-        cascade(message, label, args.copyOfRange(1, args.size))
+        val args = Arguments.parse(content)
+        val label = args.next()?.toLowerCase() ?: return
+        cascade(message, label, args)
     }
 
-    protected abstract fun cascade(message: Message, label: String, args: Array<String>)
-
-    /**
-     * Reads arguments from a given input string
-     * Arguments are delimited by whitespace unless they are wrapped in quotes.
-     * Quotes and other characters may be escaped.
-     *
-     * @param[input] The input String to read arguments from
-     * @return An [Array<String>] of the read arguments
-     */
-    private fun readArguments(input: String): Array<String> {
-        val out = mutableListOf<String>()
-        var current = String()
-        var shouldEscape = false
-        var insideQuotes = false
-        for (it in input.toCharArray()) {
-            if (shouldEscape) {
-                current += it; shouldEscape = false; continue
-            }
-            if (it == '\\') {
-                shouldEscape = true; continue
-            }
-            if (it == '\"') {
-                if (insideQuotes) {
-                    insideQuotes = false
-                    out += current
-                    current = String()
-                    continue
-                } else {
-                    insideQuotes = true; continue
-                }
-            }
-            if (it.isWhitespace() && !insideQuotes) {
-                if (current.isNotEmpty()) {
-                    out += current
-                    current = String()
-                }
-                continue
-            }
-            current += it
-        }
-        if (current.isNotEmpty()) out += current
-        return out.toTypedArray()
-    }
+    protected abstract fun cascade(message: Message, label: String, arguments: Arguments)
 }
