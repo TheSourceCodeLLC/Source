9bab1d8ac39d0ef5402c43323b32862e1cb4bad8
Hunter Wignall <hwignall2000@gmail.com>
2021-04-10 19:25:00 -0400

    Use proper version of jlatexmath

--- Extensions/Latex/build.gradle.kts
+++ Extensions/Latex/build.gradle.kts
@@ -1,5 +1,5 @@
 description = "Module responsible for the processing of LaTeX."
 
 dependencies {
-    implementation("org.scilab.forge:jlatexmath:1.0.8-SNAPSHOT")
+    implementation("org.scilab.forge:jlatexmath:1.0.7")
 }
