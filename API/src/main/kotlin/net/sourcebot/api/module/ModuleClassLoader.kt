527fa0b3b1334ea5ec79820217b713f3612d6afb
Hunter Wignall <hwignall2000@gmail.com>
2021-07-05 17:45:46 -0400

    Remove ModuleClassLoader

--- API/src/main/kotlin/net/sourcebot/api/module/ModuleClassLoader.kt
+++
@@ -1,6 +0,0 @@
-package net.sourcebot.api.module
-
-abstract class ModuleClassLoader(parent: ClassLoader) : ClassLoader(parent) {
-    final override fun findClass(name: String): Class<*> = findClass(name, true)
-    abstract fun findClass(name: String, searchParent: Boolean): Class<*>
-}
