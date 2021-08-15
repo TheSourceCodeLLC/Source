c16b664cd4cb526d392c30509de891099a62bd63
diniamo <denesrozsavolgyi@gmail.com>
2021-08-15 17:57:41 +0200

    done lol

--- Extensions/Moderation/src/main/kotlin/net/sourcebot/module/moderation/listener/MessageListener.kt
+++ Extensions/Moderation/src/main/kotlin/net/sourcebot/module/moderation/listener/MessageListener.kt
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Invite
 import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.guild.GuildBanEvent
 import net.dv8tion.jda.api.events.message.guild.GuildMessageDeleteEvent
@@ -63,6 +64,9 @@ class MessageListener : EventSubscriber<Moderation> {
     }
 
     private fun onMessageDelete(event: MessageDeleteEvent) {
+        if (isIgnored(event.channel))
+            return
+
         val (guild, authorId, content, channelId, sent) = event
         if (recentBans.asMap().containsKey(authorId)) return
         val author = event.author
@@ -99,6 +103,9 @@ class MessageListener : EventSubscriber<Moderation> {
     }
 
     private fun onMessageEdit(event: MessageEditEvent) {
+        if (isIgnored(event.channel))
+            return
+
         val (guild, author, channel, newContent, oldContent) = event
         if (recentBans.asMap().containsKey(author.id)) return
         val parent = channel.parent
@@ -144,6 +151,9 @@ class MessageListener : EventSubscriber<Moderation> {
     }
 
     private fun onMessageReceive(event: GuildMessageReceivedEvent) {
+        if (isIgnored(event.channel))
+            return
+
         if (event.author.isBot) return
         val message = event.message
         if (Source.COMMAND_HANDLER.isValidCommand(message.contentRaw) == true) return
@@ -345,5 +355,14 @@ class MessageListener : EventSubscriber<Moderation> {
             }),
             UpdateOptions().upsert(true)
         )
+    }
+
+    private fun isIgnored(channel: TextChannel?): Boolean {
+        if (channel == null)
+            return false
+
+        return configManager[channel.guild].optional<List<Long>>(
+            "moderation.ignore-channels"
+        )?.contains(channel.idLong) ?: false
     }
 }
