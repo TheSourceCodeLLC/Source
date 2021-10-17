ff4a07c69a009d118b10bc4d57f6e567334ed768
TheForbiddenAi <theforbiddenai@gmail.com>
2021-10-17 16:43:59 -0400

    Updated Role Selector Module - Removed unnecessary user selection cache in SelectionHandler
    Signed-off-by: TheForbiddenAi <theforbiddenai@gmail.com>

--- Extensions/RoleSelector/src/main/kotlin/net/sourcebot/module/roleselector/data/SelectorHandler.kt
+++ Extensions/RoleSelector/src/main/kotlin/net/sourcebot/module/roleselector/data/SelectorHandler.kt
@@ -3,8 +3,6 @@ package net.sourcebot.module.roleselector.data
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.entities.Role
-import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent
 import net.sourcebot.Source
@@ -22,8 +20,6 @@ class SelectorHandler : EventSubscriber<RoleSelector> {
         .build(object : CacheLoader<Guild, SelectorCache>() {
             override fun load(guild: Guild) = SelectorCache(mongodb.getCollection(guild.id, "role-selectors"))
         })
-
-    private val selectionCache = UserSelectionCache()
 
     operator fun get(guild: Guild): SelectorCache = menus[guild]
 
@@ -46,7 +42,7 @@ class SelectorHandler : EventSubscriber<RoleSelector> {
         val selectorName = name.substringAfter(":")
         val selector = cache[selectorName] ?: return
         val user = event.user
-        val roleIdsToAdd = event.values
+        val roleOptionsToAdd = event.values
 
         if (selector.hasPermission()) {
             val member = guild.getMember(user) ?: return
@@ -58,81 +54,22 @@ class SelectorHandler : EventSubscriber<RoleSelector> {
         }
 
         cache.verifyRoles(guild, selector)
-        val addIsEmpty = roleIdsToAdd.isEmpty()
 
-        val selectorData = selectionCache[selector] ?: mutableListOf()
-        val userSelectionData = selectorData.find { it.id == user.id }
+        val roleOptionsToRemove = selector.roleIds.toMutableList()
+        roleOptionsToRemove.removeIf { roleOptionsToAdd.contains(it) }
 
-        if (userSelectionData == null && addIsEmpty) {
-            event.reply("Please try again!").setEphemeral(true).queue()
-            return
-        }
+        val removeRoleIds = roleOptionsToRemove.map { it.substringAfter(":") }
+        val addRoles = roleOptionsToAdd.mapNotNull { guild.getRoleById(it.substringAfter(":")) }
 
-        val rolesIdsToRemove = userSelectionData?.selections?.toMutableList() ?: mutableListOf()
-        rolesIdsToRemove.removeIf { roleIdsToAdd.contains(it) }
+        val member = guild.getMember(user) ?: return
+        val memberRoles = member.roles.toMutableList()
 
-        if (addIsEmpty) selectionCache.removeSelectionData(selector, userSelectionData!!)
-        else selectionCache.addSelectionData(selector, SelectionData(user.id, roleIdsToAdd))
+        memberRoles.removeIf { removeRoleIds.contains(it.id) }
+        memberRoles.addAll(addRoles)
 
-        val removeRoles = optionIdsToRole(guild, rolesIdsToRemove)
-        val addRoles = optionIdsToRole(guild, roleIdsToAdd)
-
-        handleRoles(guild, user, addRoles, removeRoles)
+        guild.modifyMemberRoles(member, memberRoles.distinct()).queue()
 
         event.reply("Successfully updated your roles!").setEphemeral(true).queue()
     }
-
-    private fun optionIdsToRole(guild: Guild, roles: MutableList<String>) =
-        roles.map { it.substringAfter(":") }.mapNotNull { guild.getRoleById(it) }
-
-    private fun handleRoles(guild: Guild, user: User, rolesToAdd: List<Role>, rolesToRemove: List<Role>) {
-        val member = guild.getMember(user) ?: return
-        val memberRoles = member.roles.toMutableList()
-        memberRoles.removeIf { rolesToRemove.contains(it) }
-        memberRoles.addAll(rolesToAdd)
-
-        guild.modifyMemberRoles(member, memberRoles.distinct()).queue()
-    }
-
-    private class UserSelectionCache {
-
-        private val cache = CacheBuilder.newBuilder().softValues()
-            .build(object : CacheLoader<String, MutableList<SelectionData>>() {
-                override fun load(key: String): MutableList<SelectionData> = mutableListOf()
-            })
-
-        operator fun get(selector: Selector): MutableList<SelectionData>? = try {
-            cache[selector.name]
-        } catch (ex: Exception) {
-            null
-        }
-
-        fun addSelectionData(selector: Selector, selectionData: SelectionData) {
-            val currentData = get(selector) ?: mutableListOf()
-
-            currentData.removeIf { it.id == selectionData.id }
-            currentData.add(selectionData)
-
-            updateSelectionData(selector, currentData)
-        }
-
-        fun removeSelectionData(selector: Selector, selectionData: SelectionData) {
-            val currentData = get(selector) ?: mutableListOf()
-            currentData.removeIf { it.id == selectionData.id }
-
-            updateSelectionData(selector, currentData)
-        }
-
-        private fun updateSelectionData(selector: Selector, selectionData: MutableList<SelectionData>) {
-            cache.invalidate(selector.name)
-            cache.put(selector.name, selectionData)
-        }
-
-    }
-
-    private data class SelectionData(
-        val id: String,
-        var selections: MutableList<String>
-    )
 
 }
